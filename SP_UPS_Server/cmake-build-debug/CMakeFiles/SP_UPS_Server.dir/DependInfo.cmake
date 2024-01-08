ddf3f2716def1b42522e33dee691a696e5172b04
JakubHomolka <130221794+JakubHomolka@users.noreply.github.com>
2024-01-08 16:27:00 +0100

    Add files via upload

---
+++ SP_UPS_Server/cmake-build-debug/CMakeFiles/SP_UPS_Server.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/ClientManager.cpp" "CMakeFiles/SP_UPS_Server.dir/ClientManager.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/ClientManager.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/Game.cpp" "CMakeFiles/SP_UPS_Server.dir/Game.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/Game.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/GameLogic.cpp" "CMakeFiles/SP_UPS_Server.dir/GameLogic.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/GameLogic.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/GameManager.cpp" "CMakeFiles/SP_UPS_Server.dir/GameManager.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/GameManager.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/Player.cpp" "CMakeFiles/SP_UPS_Server.dir/Player.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/Player.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/Request.cpp" "CMakeFiles/SP_UPS_Server.dir/Request.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/Request.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/Response.cpp" "CMakeFiles/SP_UPS_Server.dir/Response.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/Response.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/Server.cpp" "CMakeFiles/SP_UPS_Server.dir/Server.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/Server.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/Utils.cpp" "CMakeFiles/SP_UPS_Server.dir/Utils.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/Utils.cpp.o.d"
+  "/mnt/c/Users/werto/CLionProjects/SP_UPS_Server/main.cpp" "CMakeFiles/SP_UPS_Server.dir/main.cpp.o" "gcc" "CMakeFiles/SP_UPS_Server.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
